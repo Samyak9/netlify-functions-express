c13e9b899193e00799621a3084fe24295cbcdf48
Samyak <samyakrox9@gmail.com>
2020-07-15 21:02:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -111,6 +111,6 @@ transporter.sendMail(mailOptions, function(error, info){
 });
 });
 
-app.listen(3000, function() {
-  console.log("Server has started");
+app.listen (process.env.PORT || 3000 , function () {
+  console.log ("Server is running on port 3000.");
 });
